0f467c8bfbb0aef79dd3f40049143ecc02e6d938
Hector <hectorthoegersen@gmail.com>
2020-11-27 16:12:08 +0100

    Update

--- src/detect_eyes_video_1.py
+++ src/detect_eyes_video_1.py
@@ -18,24 +18,24 @@ while(cap.isOpened()):
     color = frame
     subtract = copy.copy(frame)
 
-    if (ret == True):
-        for x in range(color.shape[0]):
-            for y in range(color.shape[1]):
-                if (color[x, y, 1] < 150 and color[x, y, 2] > 80 and color[x, y, 2] < 255):
-                    color[x, y, 1] = 255
-                    color[x, y, 2] = 255
-                    color[x, y, 0] = 255
-                elif (color[x, y, 0] < 50):
-                    color[x, y, 1] = 255
-                    color[x, y, 2] = 255
-                    color[x, y, 0] = 255
-                elif (color[x, y, 0] > 135 and color[x, y, 1] > 120 and color[x, y, 2] > 100):
-                    color[x, y, 1] = 0
-                    color[x, y, 2] = 0
-                    color[x, y, 0] = 0
-                elif (color[x, y, 0] > 20 and color[x, y, 0] < 80 and color[x, y, 1] > 20 and color[x, y, 2] > 20):
-                    color[x, y, 2] = 255
-                    red += 1
+    for x in range(color.shape[0]):
+        for y in range(color.shape[1]):
+            if (color[x, y, 1] < 150 and color[x, y, 2] > 80 and color[x, y, 2] < 255):
+                color[x, y, 1] = 255
+                color[x, y, 2] = 255
+                color[x, y, 0] = 255
+            elif (color[x, y, 0] < 50):
+                color[x, y, 1] = 255
+                color[x, y, 2] = 255
+                color[x, y, 0] = 255
+            elif (color[x, y, 0] > 135 and color[x, y, 1] > 120 and color[x, y, 2] > 100):
+                color[x, y, 1] = 0
+                color[x, y, 2] = 0
+                color[x, y, 0] = 0
+            elif (color[x, y, 0] > 20 and color[x, y, 0] < 80 and color[x, y, 1] > 20 and color[x, y, 2] > 20):
+                color[x, y, 2] = 255
+                red += 1
+
     if (red > 2000):
         print("eyes are open")
     elif (red < 2000):
@@ -43,12 +43,12 @@ while(cap.isOpened()):
 
     subtract_median = copy.copy(color)
     print(red)
-
     cv2.imshow("Hector", color)
     cv2.imshow("jojo", subtract)
+    print((time.time() - starttime))
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-print((time.time() - starttime))
